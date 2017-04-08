98a6f638d44244c06ae4365f12848f339f9f3269
benjadahl <benjadahl@gmail.com>
2017-04-08 20:19:14 +0200

    Added Katex

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 var algebrite = require("algebrite");
+var katex = require("katex");
 
 console.log("Welcome to BenjaMath");
 
 console.log(algebrite.eval("2+2x*x").toString());
+
+katex.render(algebrite.eval("2+2x*x").toString(), document.getElementById("renderElement"));
