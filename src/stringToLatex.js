fdd30f9dcdf96399f7d2d90d89329d2fcaef4605
benjadahl <benjadahl@gmail.com>
2017-04-11 19:29:35 +0200

    Did some work on stringToLatex

--- src/stringToLatex.js
+++ src/stringToLatex.js
@@ -1,15 +1,15 @@
-//Homemade string to latex parser, not in use yet
+//Homemade string to latex parser
 
-var matchParentheses = function (input, startIndex) {
+var matchParentheses = function (input, startIndex, reverse = false) {
   let depth = 0;
-  for (i = startIndex; i < input.length; i++) {
+  for (i = startIndex; 0 < i && i < input.length; i = reverse ? i - 1 : i + 1) {
     if (input[i] === "(") {
       depth++;
     } else if (input[i] === ")") {
       depth--;
-      if (depth === 0) {
-        return i;
-      }
+    }
+    if (depth === 0) {
+      return i;
     }
   }
   return -1;
@@ -35,15 +35,69 @@ var replaceAtIndex = function (input, index, replacement) {
 }
 
 var stringToLatex = function (input) {
+  console.log(input);
+  let occurences;
+
   //Turn exponential parentheses into latex notation
-  let occurences = searchAll(input,"^(");
+  occurences = searchAll(input,"^(");
   for (let i = 0; i < occurences.length; i++) {
     let endParentheses = matchParentheses(input, occurences[i] + 1);
     input = replaceAtIndex(input, occurences[i] + 1, "{");
     input = replaceAtIndex(input, endParentheses, "}");
   }
+
+  //Replace all stars with \cdot
+  occurences = searchAll(input, "*");
+  for (let i = 0; i < occurences.length; i++) {
+    input = input.replace("*", " \\cdot ");
+  }
+
   //Turn parentheses around numerator and denomenator into latex notation
-  
+  occurences = searchAll(input, "/");
+  for (let i = 0; i < occurences.length; i++) {
+    let index = occurences[i];
+    let start;
+    let end;
+
+    //Search for end of each term
+    //Search left
+    for (let j = index - 1; j >= 0; j--) {
+      let isPlus = input[j] === "+";
+      let isMinus = input[j] === "-";
+      let isSlash = input[j] === "/";
+      let isStar = input[j] === "*";
+
+      if (isPlus || isMinus || isSlash || isStar) {
+        start = j + 1;
+        break;
+      } else if (input[j] === ")") {
+        start = matchParentheses(input, j, true);
+        break;
+      } else if (input[j] === "(") {
+        start = j + 1;
+        break;
+      }
+    }
+    //Search right
+    for (let j = index + 1; j < input.length; j++) {
+      let isPlus = input[j] === "+";
+      let isMinus = input[j] === "-";
+      let isSlash = input[j] === "/";
+      let isStar = input[j] === "*";
+
+      if (isPlus || isMinus || isSlash || isStar) {
+        end = j - 1;
+        break;
+      } else if (input[j] === "(") {
+        end = matchParentheses(input, j, false);
+      } else if (input[j] === ")") {
+        end = j - 1;
+      }
+    }
+    console.log("For i " + i + " start: " + start + " end: " + end);
+    //input = input.replace("/", " x ");
+  }
+
   return input;
 }
 
