9bc79650badd20ff4060e78c7f3eb7d80ed0f132
benjadahl <benjadahl@gmail.com>
2017-04-12 13:09:30 +0200

    Attempted to implement mathquill with ckeditor but horribly failed

--- src/index.js
+++ src/index.js
@@ -6,12 +6,9 @@ var algebra = require("algebra.js");
 var math = require("mathjs");
 var stringToLatex = require("./stringToLatex.js");
 
-$(window).on("load", function(){
-  var MQ = MathQuill.getInterface(2);
-  var mathquillTest = document.getElementById("mathquillTest");
-  MQ.MathField(mathquillTest);
-});
-
+var MQ = MathQuill.getInterface(2);
+var mathquillTest = document.getElementById("mathquillTest");
+MQ.MathField(mathquillTest);
 
 console.log("Welcome to BenjaMath");
 
@@ -19,6 +16,25 @@ console.log(stringToLatex("-1/2 (b^2 / (a^2) - 4 c / a)^(1/2) - b / (2 a)^(3*(4+
 
 CKEDITOR.replace("editor");
 
+//Modify editor
+//CKEDITOR.config.allowedContent = true;
+
+var editor = CKEDITOR.instances.editor;
+
+var equations = 0;
+
+editor.addCommand( 'insertMathquill', {
+  exec: function( editor ) {
+    editor.insertHtml( '<span class="math" contenteditable="false">equation' + equations + '</span>' );
+    equations++;
+  }
+});
+
+editor.ui.addButton("mathQuill", {
+  label: "Insert Equation",
+  command: 'insertMathquill',
+  toolbar: 'insert'
+});
 
 for (var i in CKEDITOR.instances) {
   let previewElement = document.getElementById("preview");
@@ -41,13 +57,13 @@ function renderPreview (element) {
     console.log("Evaluating");
     let element = mathTags[0];
     console.log(algebrite.eval(element.innerHTML).toString());
-    let rs = stringToLatex(algebrite.eval(element.innerHTML));
-    let ls;
-    try {
+    let rs = algebrite.eval(element.innerHTML);
+    let ls = element.innerHTML;
+    /*try {
       ls = stringToLatex(element.innerHTML);
     } catch (err) {
       ls = element.innerHTML;
-    }
+    }*/
 
     let replaceString = katex.renderToString(ls + "=" + rs);
     $(element).replaceWith(replaceString);
