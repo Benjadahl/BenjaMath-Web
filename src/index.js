3992193de9b208e954aa659e78f0f3bb4b5c4465
benjadahl <benjadahl@gmail.com>
2017-05-03 19:08:28 +0200

    More mathquill intergration

--- src/index.js
+++ src/index.js
@@ -5,69 +5,103 @@ require("ckeditor");
 var algebra = require("algebra.js");
 var math = require("mathjs");
 var stringToLatex = require("./stringToLatex.js");
+const AlgebraLatex = require("algebra-latex");
 
 var MQ = MathQuill.getInterface(2);
-var mathquillTest = document.getElementById("mathquillTest");
-MQ.MathField(mathquillTest);
 
 console.log("Welcome to BenjaMath");
 
-console.log(stringToLatex("-1/2 (b^2 / (a^2) - 4 c / a)^(1/2) - b / (2 a)^(3*(4+9))"));
+//console.log(stringToLatex("-1/2 (b^2 / (a^2) - 4 c / a)^(1/2) - b / (2 a)^(3*(4+9))"));
 
-CKEDITOR.replace("editor");
+var MathQuills = [];
+var MqCount = 0;
 
 //Modify editor
-//CKEDITOR.config.allowedContent = true;
+CKEDITOR.config.allowedContent = true;
+$(document).ready(function () {
+  var editor = CKEDITOR.instances.editor;
+  editor.addCommand( 'insertMathquill', {
+    exec: function( editor ) {
+      let id = 'Mq' + MqCount;
+      editor.insertHtml( '<p contenteditable="false">&#8291<span id="' + id + '" contenteditable="false">placeholder</span></p><p></p>' );
+      MqCount++;
+      MathQuills.push(MQ.MathField(document.getElementById(id),
+      {
+        handlers: {
+          enter: function () {
+            console.log("test");
+          }
+        }
+      }).select().focus());
+      //Pretty bad hack - it makes the box editable and empty
+      let currentQuill = MathQuills[MathQuills.length - 1];
+      setTimeout(function(){
+        currentQuill.clearSelection();
+        currentQuill.latex("");
+      }, 200);
 
-var editor = CKEDITOR.instances.editor;
+      //Also hack when clicked
+      currentQuill.el().addEventListener("click", function(){
+        if (currentQuill.latex() === "") {
+          currentQuill.latex("placeholder");
+        }
+        currentQuill.select();
+        setTimeout(function(){
+          currentQuill.clearSelection();
+          if (currentQuill.latex() === "placeholder") {
+            currentQuill.latex("");
+          }
+        }, 200);
+      });
+    }
+  });
 
-var equations = 0;
+  editor.ui.addButton("mathQuill", {
+    label: "Insert Equation",
+    command: 'insertMathquill',
+    toolbar: 'insert'
+  });
 
-editor.addCommand( 'insertMathquill', {
-  exec: function( editor ) {
-    editor.insertHtml( '<span class="math" contenteditable="false">equation' + equations + '</span>' );
-    equations++;
-  }
-});
-
-editor.ui.addButton("mathQuill", {
-  label: "Insert Equation",
-  command: 'insertMathquill',
-  toolbar: 'insert'
-});
-
-for (var i in CKEDITOR.instances) {
-  let previewElement = document.getElementById("preview");
-
-  CKEDITOR.instances[i].on("change", function() {
+  /*let previewElement = document.getElementById("preview");
+  editor.on("change", function() {
     renderPreview(previewElement);
   });
-  CKEDITOR.instances[i].on("instanceReady", function() {
+
+  editor.on("instanceReady", function() {
     renderPreview(previewElement);
-  });
-}
+  });*/
+});
+
+
 
 parser = new DOMParser();
 
 function renderPreview (element) {
   var eData = CKEDITOR.instances.editor.getData();
   var data = parser.parseFromString(eData, "text/html");
-  var mathTags = data.getElementsByTagName("em");
-  while (mathTags.length > 0) {
+  //var mathTags = data.getElementsByClassName("math");
+
+  for (var i = 0; i < MathQuills.length; i++) {
+    let latex = MathQuills[i].latex();
+    let stringMath = new AlgebraLatex(latex).toMath();
+    console.log(algebrite.eval(stringMath).toString());
+  }
+
+  /*while (mathTags.length > 0) {
     console.log("Evaluating");
     let element = mathTags[0];
     console.log(algebrite.eval(element.innerHTML).toString());
     let rs = algebrite.eval(element.innerHTML);
     let ls = element.innerHTML;
-    /*try {
+    try {
       ls = stringToLatex(element.innerHTML);
     } catch (err) {
       ls = element.innerHTML;
-    }*/
+    }
 
     let replaceString = katex.renderToString(ls + "=" + rs);
     $(element).replaceWith(replaceString);
-  }
+  }*/
 
   element.innerHTML = data.getElementsByTagName("body")[0].innerHTML;
 }
